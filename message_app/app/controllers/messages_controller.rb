6bd7a6706d9d0bb77ad5b8c211f07ec05d282231
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-03-23 05:52:39 +0000

    messagesコントローラーを作成

---
+++ message_app/app/controllers/messages_controller.rb
@@ -0,0 +1,13 @@
+class MessagesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- message_app/config/routes.rb
+++ message_app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+  get 'messages/new'
+  get 'messages/edit'
+  get 'messages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
